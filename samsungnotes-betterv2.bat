ab463e70ce29894e923f86f903df63ed2aceced1
MANAS C <78652689+iknothing@users.noreply.github.com>
2024-02-17 09:11:47 +0530

    (recommended)

--- samsungnotes-betterv2.bat
+++ samsungnotes-betterv2.bat
@@ -3,8 +3,7 @@ set "params=%*" && cd /d "%~dp0" && pushd "%~dp0" && ( if exist "%temp%\getadmin
 :: Store the previous system product name and system manufacturer
 for /f "tokens=2*" %%a in ('reg query "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemProductName ^| find "REG_SZ"') do set "prev_product_name=%%b"
 for /f "tokens=2*" %%a in ('reg query "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemManufacturer ^| find "REG_SZ"') do set "prev_manufacturer=%%b"
-
-
+
 :: Set the new system product name and system manufacturer
 reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemProductName /t REG_SZ /d "NP960XFG-KC4UK" /f
 reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemManufacturer /t REG_SZ /d "Samsung" /f
